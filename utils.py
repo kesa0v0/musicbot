c9b58d26affe620a652ba7e3d06bb85d8977ace0
kesa <kesa0v0@gmail.com>
2025-08-07 17:20:03 +0900

    yt-dlp를 사용하여 관련 비디오 검색 기능으로 변경 및 YOUTUBE_API_KEY 관련 코드 제거

--- utils.py
+++ utils.py
@@ -6,26 +6,6 @@ logger = logging.getLogger(__name__)
 
 def get_related_videos(video_id, max_results=5):
     try:
-        YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
-        if not YOUTUBE_API_KEY:
-            logger.warning("[utils] YOUTUBE_API_KEY is not set.")
-            return []
-        
-        # Removed googleapiclient.discovery related code as per user's request to use only yt-dlp
-        # youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
-        # 
-        # response = youtube.search().list(
-        #     part='snippet',
-        #     relatedToVideoId=video_id,
-        #     type='video',
-        #     maxResults=max_results
-        # ).execute()
-        # 
-        # return [
-        #     {'title': item['snippet']['title'], 'id': item['id']['videoId']}
-        #     for item in response.get('items', [])
-        # ]
-        
         # Using yt-dlp as the primary method for related videos
         with youtube_dl.YoutubeDL({'quiet': True, 'extract_flat': True}) as ydl:
             mix_url = f"https://www.youtube.com/watch?v={video_id}&list=RD{video_id}"
